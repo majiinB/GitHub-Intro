9b1d65ba39e8bb709232b4a2b23247c694753112
Arthur Artugue <artuguearthur@gmail.com>
2025-10-18 21:37:21 +0800

    modified hello.py to take user input and print their name

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+def main():
+	name = input("Enter your name: ")
+	if name.strip():
+		print(f"Hello, {name}!")
+	else:
+		print("Hello!")
+
+
+if __name__ == "__main__":
+	main()
